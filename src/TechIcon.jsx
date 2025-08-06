267a1664b5f1268ad37788895bbf8aa8a096f05c
Hamda-Saeed <hamdasaeed915@gmail.com>
2025-08-07 03:48:49 +0500

    update tech Icon

--- src/TechIcon.jsx
+++ src/TechIcon.jsx
@@ -1,7 +1,17 @@
 const TechIcon = ({ icon }) => {
   return (
-    <div className="flex-none md:w-32 md:h-32 w-20 h-20 bg-black-300 flex-center gradient-border marquee-item hover:-translate-y-3 transition-all duration-700">
-      <img src={icon.image} alt={icon.name} className="md:size-16 size-10" />
+    <div 
+      // These classes control the overall size of the icon container
+      // w-16 and h-16 for smaller screens, md:w-20 and md:h-20 for medium screens and up
+      className="flex-none w-16 h-16 md:w-20 md:h-20 bg-black-300 flex-center gradient-border marquee-item hover:-translate-y-3 transition-all duration-700"
+    >
+      <img 
+        src={icon.image} 
+        alt={icon.name} 
+        // These classes control the size of the image inside the container
+        // size-8 for smaller screens, md:size-10 for medium screens and up
+        className="size-8 md:size-10" 
+      />
     </div>
   );
 };
